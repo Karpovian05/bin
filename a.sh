b15e959757fcd73c0e3144c1f0edcdf950c19b75
Karpovian05 <104993350+Karpovian05@users.noreply.github.com>
2022-05-24 15:56:32 +0800

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-jffs2reset -y
-fw_setenv dropbear_mode
-fw_setenv dropbear_password
-fw_setenv dropbear_key_type
+#jffs2reset -y
+#fw_setenv dropbear_mode
+#fw_setenv dropbear_password
+#fw_setenv dropbear_key_type
 #echo "Changing IMEI..."
 #ubus call version set_atcmd_info '{"atcmd":"AT*PROD=2"}' > /dev/null 2&>1
 #ubus call version set_atcmd_info '{"atcmd":"AT*MRD_IMEI=D"}' > /dev/null 2&>1
@@ -12,12 +12,12 @@ fw_setenv dropbear_key_type
 firmware2=$(cat /proc/mtd | grep firmware2 | awk '{print $1}') 
 if [ $firmware2 == 'mtd7:' ] 
 then 
-wget http://github.com/Karpovian05/bin/raw/main/a.bin -O /tmp/firmware.bin
+#wget http://github.com/Karpovian05/bin/raw/main/a.bin -O /tmp/firmware.bin
 echo "Wait for the modem to reboot..."
-mtd -r write /tmp/firmware.bin /dev/mtd4
+mtd -r write /tmp/a.bin /dev/mtd4
 exit
 fi 
-wget http://github.com/Karpovian05/bin/raw/main/a.bin -O /tmp/firmware.bin
+#wget http://github.com/Karpovian05/bin/raw/main/a.bin -O /tmp/firmware.bin
 echo "Wait for the modem to reboot..."
-mtd -r write /tmp/firmware.bin /dev/mtd5
+mtd -r write /tmp/a.bin /dev/mtd5
 exit
